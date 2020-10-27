6d7a4822e241d511a2733935525f19dd8f2b7fe4
vanessalevano2020 <lesvanell@gmail.com>
2020-10-27 01:21:14 -0300

    Estilos en Employer Page

--- pages/employer/index.js
+++ pages/employer/index.js
@@ -1,49 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View, Alert } from 'react-native';
+import { Button, StyleSheet, Text, View, Alert, Image } from 'react-native';
 
 export default function Employeer({navigation}){
     return(
-        <View>
-            <Text>This is miTablero Page</Text>
-            <View>
-                <Button
-                    title="Editar datos"
-                    
-                    // onPress={()=> {
-                    //     navigation.navigate("")
-                    // }}
-                    onPress={() => Alert.alert('Editar datos')}   
-                />
-
-                <Button
-                    title="ABM Empleados"
-                    
-                    // onPress={()=> {
-                    //     navigation.navigate("")
-                    // }}
-                    onPress={() => Alert.alert('ABM Empleados')}  
-                />
-
-                <Button
-                    title="Asistencias"
-                    
-                    // onPress={()=> {
-                    //     navigation.navigate("")
-                    // }}
-                    onPress={() => Alert.alert('Asistencias')}  
-                />
-
-                <Button
-                    title="Informes"
-                    
-                    // onPress={()=> {
-                    //     navigation.navigate("")
-                    // }}
-                    onPress={() => Alert.alert('Informes')}  
-                />
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.headerLeft}>
+                    <Image source={require('../../assets/images/employer.png')} style={styles.logo}/>
+                </View>
+                <View style={styles.headerRight}>
+                    <Button 
+                        title="Editar datos"
+                        // onPress={()=> {
+                        //     navigation.navigate("")
+                        // }}
+                        onPress={() => Alert.alert('Editar datos')}   
+                    />
+                </View>
             </View>
-           
+            {/* <Text style={styles.title}>This is Employeer Page</Text> */}
+            <View style={styles.body}>
+                <View style={styles.division}>
+                    <View style={styles.row}>
+                        <Button
+                            title="ABM Empleados"
+                            
+                            // onPress={()=> {
+                            //     navigation.navigate("")
+                            // }}
+                            onPress={() => Alert.alert('ABM Empleados')}  
+                        />
+                    </View>
+                    <View style={styles.row}>
+                        <Button
+                            title="Asistencias"
+                            
+                            // onPress={()=> {
+                            //     navigation.navigate("")
+                            // }}
+                            onPress={() => Alert.alert('Asistencias')}  
+                        />
+                    </View>
+                </View>
+                <View style={styles.division}>
+                    <View style={styles.row}>
+                        <Button
+                            title="Informes"
+                            
+                            // onPress={()=> {
+                            //     navigation.navigate("")
+                            // }}
+                            onPress={() => Alert.alert('Informes')}  
+                        />
+                    </View>
+                    <View style={styles.row}>
+                        <Button
+                            title="Otros"
+                            
+                            // onPress={()=> {
+                            //     navigation.navigate("")
+                            // }}
+                            onPress={() => Alert.alert('Otro')}  
+                        />
+                    </View>  
+                </View>
+            </View>    
         </View>
     )
 }
@@ -51,16 +73,47 @@ export default function Employeer({navigation}){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        marginHorizontal: 16,
+        flexDirection: 'column',
       },
+    header: {
+        flex: 0.3,
+        flexDirection: 'row',
+        marginTop: 40,
+      },
+    headerLeft: {
+        flex: 1,
+      },
+    headerRight: {
+        flex: 1,
+      }, 
+
     title: {
         textAlign: 'center',
         marginVertical: 8,
       },
-      fixToText: {
+
+    row: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginVertical: 200,
-      },
+        // textAlign: 'center',
+        marginVertical: 20,
+        marginTop:100,
+        marginLeft: 20,
+    },
+    logo: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        resizeMode: 'contain',
+    },
+
+    body: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+
+    division: {
+        flex: 1,
+        flexDirection: 'column',
+    },
 });
