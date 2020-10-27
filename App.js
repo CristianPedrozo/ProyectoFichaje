32e89d3582886bddbf006c6a19d0e3c7536edb2d
javieter <olmedo_javier@hotmail.com>
2020-10-27 18:37:12 -0300

    agregado de pantalla de Vane

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Employee from "./pages/employee";
+import Employer from "./pages/employer";
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
     //   <Text>Open up App.js to start working on your app!</Text>
     //   <StatusBar style="auto" />
     // </View>
-    <Employee/>
+    <Employer/>
   );
 }
 
