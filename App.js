d4b55e6edbf972f6397858affeda5b035eaaf05c
javieter <olmedo_javier@hotmail.com>
2020-10-27 15:00:15 -0300

    pruebas con flex

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Button, View, Text, Alert } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import Employee from "./pages/employee";
 
-const App = () => (
+export default function App() {
+  return (
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
     <Employee/>
-);
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
     justifyContent: 'center',
-    marginHorizontal: 16,
   },
 });
-
-export default App;
--- pages/employee/index.js
+++ pages/employee/index.js
@@ -1,41 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { Button, StyleSheet, Text, TextInput, View, Alert } from 'react-native';
+import { StyleSheet, Text, View, Alert, Button, Image } from 'react-native';
 
 
 export default function Employee() {
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Vista de empleado</Text>
-            <StatusBar style="auto" />
-            <View style={styles.fixToText}>
-                <Button
-                title="Fichar"
-                onPress={() => Alert.alert('Debería fichar')}
-                />
-                <Button
-                title="Mi tablero"
-                onPress={() => Alert.alert('Debería mostrar menú de opciones')}
-            />
-      </View>
-            
+      <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.headerLeft}>
+          <Image source={require('../../assets/logo.png')} style={styles.logo}></Image>
         </View>
-    );
+        <View style={styles.headerRigth}>
+          <Text>Empresa S.A.</Text>
+        </View>
+      </View>
+      
+      <View style={styles.body}>
+        <Text>Datos del empleado</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <View style={[styles.footerLeft, styles.button]}>
+          <Button
+            title="Fichar"
+            onPress={() => Alert.alert('Debería abrir lector QR')}
+            />
+          </View>
+        <View style={[styles.footerRigth, styles.button]}>
+          <Button
+            title="Mi tablero"
+            onPress={() => Alert.alert('Debería mostrar menú de opciones')}
+          />
+        </View>
+      </View>
+
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        marginHorizontal: 16,
-      },
-    title: {
-        textAlign: 'center',
-        marginVertical: 200,
-      },
-      fixToText: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-      },
+  container: {
+    flex: 1,
+    flexDirection : 'column'
+  },
+  header: {
+    flex: 0.7,
+    flexDirection: 'row'
+  },
+  headerLeft : {
+    flex: 1,
+    marginTop: 40,
+    marginLeft: 10
+  },
+  headerRigth : {
+    flex: 0.9,
+    justifyContent: 'center'
+  },
+  body : {
+    flex: 1,
+    backgroundColor : 'white',
+    justifyContent: 'center',
+  },
+  footer : {
+    flex: 0.7,
+    flexDirection: 'row'
+  },
+  footerLeft : {
+    flex: 1,
+  },
+  footerRigth : {
+    flex: 1,
+  },
+  button : {
+    justifyContent: 'center',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    alignItems: 'center'
+  },
 });
