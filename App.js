8eab499dd949e68dbc1e900b0e23d08fd46951a6
javieter <olmedo_javier@hotmail.com>
2020-11-19 08:10:36 -0300

    carga de datos de empleado OK

--- App.js
+++ App.js
@@ -5,27 +5,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import Employee from "./pages/employee";
 import Employer from "./pages/employer";
 
-const API_URL = 'https://tranquil-dusk-24173.herokuapp.com/api/usuarios/lesvanell@gmail.com';
-
-
 export default function App() {
 
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error("hubo un error",error));
-  }, []);
-  console.log(data.name.first)
+  
   return (
-    
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <Employee name={data.name}></Employee>
+    <Employee/>
   );
 }
 
--- pages/employee/index.js
+++ pages/employee/index.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Alert, Button, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Alert, Button, Image, ActivityIndicator } from 'react-native';
 
+//const API_URL = 'https://tranquil-dusk-24173.herokuapp.com/api/usuarios/lesvanell@gmail.com';
+const API_URL = 'https://stark-atoll-54719.herokuapp.com/api/usuarios/admin@admin'
 
-export default function Employee({name}) {
+export default function Employee() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState();
 
-    return (
-      <View style={styles.container}>
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <View style={styles.container}>
       <View style={styles.header}>
         <View style={styles.headerLeft}>
           <Image source={require('../../assets/logo.png')} style={styles.logo}></Image>
@@ -15,20 +27,23 @@ export default function Employee({name}) {
           <Text>Empresa S.A.</Text>
         </View>
       </View>
-      
+
       <View style={styles.body}>
-        <Text>
-          {name.first} {name.last}
-        </Text>
+        {isLoading ? <ActivityIndicator /> : (
+          <Text>
+            {data.name.first} {data.name.last} {data.adress.street}
+          </Text>
+        )}
       </View>
+
 
       <View style={styles.footer}>
         <View style={[styles.footerLeft, styles.button]}>
           <Button
             title="Fichar"
             onPress={() => Alert.alert('Debería abrir lector QR')}
-            />
-          </View>
+          />
+        </View>
         <View style={[styles.footerRigth, styles.button]}>
           <Button
             title="Mi tablero"
@@ -44,38 +59,38 @@ export default function Employee({name}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection : 'column'
+    flexDirection: 'column'
   },
   header: {
     flex: 0.7,
     flexDirection: 'row',
     backgroundColor: 'orange'
   },
-  headerLeft : {
+  headerLeft: {
     flex: 1,
     marginTop: 40,
     marginLeft: 10
   },
-  headerRigth : {
+  headerRigth: {
     flex: 0.9,
     justifyContent: 'center'
   },
-  body : {
+  body: {
     flex: 1,
-    backgroundColor : 'white',
+    backgroundColor: 'white',
     justifyContent: 'center',
   },
-  footer : {
+  footer: {
     flex: 0.7,
     flexDirection: 'row'
   },
-  footerLeft : {
+  footerLeft: {
     flex: 1,
   },
-  footerRigth : {
+  footerRigth: {
     flex: 1,
   },
-  button : {
+  button: {
     justifyContent: 'center',
     marginLeft: 5,
     marginRight: 5
