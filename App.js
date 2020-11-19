2c515a27076c597b5c7014d653fc89d4c8e42ce9
unknown <hhv1996@gmail.com>
2020-11-19 01:00:44 -0300

    QR con url javi funcionando

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import VistaAsistenciaQR from "./pages/QRLector/indexFichar";
 
+const crudEmpleado={"name":{"first":"Admin","last":"admin"},
+"adress":{"street":"Calle Falsa","number":1234,"floor":5,"apartment":"C"},
+"_id":"5fab59367fa94b820060a637",
+"phone":"1511223344",
+"email":"admin@admin",
+"secret":"WjJGaWFWOXNZVzFoYzE5cmNHRkFlV0ZvYjI4dVkyOXQ5MTI2",
+"imagePatch":"TBD",
+"isAdmin":true,
+"checkIn":1000,
+"checkOut":1900,"__v":0};
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <VistaAsistenciaQR usuario={crudEmpleado}></VistaAsistenciaQR>
   );
 }
 
